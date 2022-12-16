cbf1704d661dad4823ece547c67dae7284488660
Christine Han <christinehan726@gmail.com>
2022-12-15 22:47:15 -0800

    added new columns

--- pokemon/models.py
+++ pokemon/models.py
@@ -48,17 +48,22 @@ class User(db.Model, UserMixin):
 class Pokemon(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     name = db.Column(db.String(100), nullable=False)
+    type = db.Column(db.String(100), nullable=True)
+    description = db.Column(db.Text, nullable=False)
+    image_file = db.Column(
+        db.String(120),
+        nullable=True
+    )
     date_caught = db.Column(
         db.DateTime,
         nullable=True,
     )
-    description = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     date_created = db.Column(
         db.DateTime,
         nullable=False,
         default=datetime.utcnow
     )
     def __repr__(self):
-        return f"Pokemon('{self.title}', '{self.date_posted}')"
+        return f"Pokemon('{self.name}', '{self.date_created}')"
