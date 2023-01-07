51b65737e89ddd104342aef7fb2d4bb9e71a7133
Christine Han <christinehan726@gmail.com>
2023-01-06 17:09:15 -0800

    added new form fields

--- pokemon/pokemons/forms.py
+++ pokemon/pokemons/forms.py
@@ -1,8 +1,13 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import (
+    FileAllowed,
+    FileField,
+)
 from wtforms import (
     StringField,
     SubmitField,
     TextAreaField,
+    DateTimeField,
 )
 from wtforms.validators import DataRequired
 
@@ -10,5 +15,11 @@ from wtforms.validators import DataRequired
 class PokemonForm(FlaskForm):
 
     name = StringField('Name', validators=[DataRequired()])
+    type = StringField('Type')
     description = TextAreaField('Description', validators=[DataRequired()])
+    image_file = FileField(
+        'Upload Pokemon picture',
+        validators=[FileAllowed(['jpg', 'png'])]
+    )
+    date_caught = DateTimeField('Date Caught')
     submit = SubmitField('Save')
