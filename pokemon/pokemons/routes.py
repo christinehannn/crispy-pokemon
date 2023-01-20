f95618c9eb25547d725c4dc73d95c2c3eb1d1eb4
Christine Han <christinehan726@gmail.com>
2023-01-20 09:43:34 -0800

    added type, date caught, and image file to update and new pokemon

--- pokemon/pokemons/routes.py
+++ pokemon/pokemons/routes.py
@@ -15,6 +15,7 @@ from flask_login import (
 from pokemon import db
 from pokemon.models import Pokemon
 from pokemon.pokemons.forms import PokemonForm
+from pokemon.users.utils import save_picture
 
 pokemons = Blueprint('pokemons', __name__)
 
@@ -26,9 +27,14 @@ def new_pokemon():
     if form.validate_on_submit():
         pokemon = Pokemon(
             name=form.name.data,
+            date_caught=form.date_caught.data,
+            type=form.type.data,
             description=form.description.data,
-            trainer=current_user
+            trainer=current_user,
         )
+        if form.image_file.data:
+            image_file = save_picture(form.image_file.data)
+            pokemon.image_file = image_file
         db.session.add(pokemon)
         db.session.commit()
         flash('Your Pokemon has been created!', 'success')
@@ -44,7 +50,7 @@ def new_pokemon():
 @pokemons.route('/pokemon/<int:pokemon_id>')
 def pokemon(pokemon_id):
     pokemon = Pokemon.query.get_or_404(pokemon_id)
-    return render_template('pokemon.html', title=pokemon.title, pokemon=pokemon)
+    return render_template('pokemon.html', title=pokemon.name, pokemon=pokemon)
 
 
 @pokemons.route('/pokemon/<int:pokemon_id>/update', methods=['GET', 'POST'])
@@ -56,13 +62,21 @@ def update_pokemon(pokemon_id):
     form = PokemonForm()
     if form.validate_on_submit():
         pokemon.name = form.name.data
+        pokemon.date_caught = form.date_caught.data
+        pokemon.type = form.type.data
         pokemon.description = form.description.data
+        if form.image_file.data:
+            image_file = save_picture(form.image_file.data)
+            pokemon.image_file = image_file
         db.session.commit()
         flash('Your pokemon has been updated!', 'success')
         return redirect(url_for('pokemons.pokemon', pokemon_id=pokemon.id))
     elif request.method == 'GET':
         form.name.data = pokemon.name
+        form.date_caught.data = pokemon.date_caught
+        form.type.data = pokemon.type
         form.description.data = pokemon.description
+        form.image_file.data = pokemon.image_file
     return render_template(
         'create_pokemon.html',
         title='Update Pokemon',
